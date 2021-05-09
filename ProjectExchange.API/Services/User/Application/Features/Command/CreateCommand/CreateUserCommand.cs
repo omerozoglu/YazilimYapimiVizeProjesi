d695f7e90f5868277ce688045efcbda50b469f4a
omerozoglu <omerozoglu.1@gmail.com>
2021-05-09 17:09:17 +0300

    :: Story Implementation, User API Layer, Application Layer, Ifrastructure Layer {MongoDB Setting And Testing}

--- ProjectExchange.API/Services/User/Application/Features/Command/CreateCommand/CreateUserCommand.cs
+++ ProjectExchange.API/Services/User/Application/Features/Command/CreateCommand/CreateUserCommand.cs
@@ -7,6 +7,9 @@ namespace Application.Features.Command.CreateCommand {
         public string Username { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public int AthLvl { get; set; }
+        public int AccountType { get; set; }
+        public string TCNumber { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
     }
 }
