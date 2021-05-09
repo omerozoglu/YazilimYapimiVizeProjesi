fd9cd80d71a511568efb79af5a927e2d2b50f726
omerozoglu <omerozoglu.1@gmail.com>
2021-05-09 16:32:30 +0300

    :: Story Implementation, User API Layer {-}

---
+++ ProjectExchange.API/Services/User/API/Controllers/UserController.cs
@@ -0,0 +1,81 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Application.Features.Command.CreateCommand;
+using Application.Features.Command.DeleteCommand;
+using Application.Features.Command.UpdateCommand;
+using Application.Features.Queries.Get;
+using Application.Features.Queries.GetList;
+using Application.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers {
+    [ApiController]
+    // api/v1/User
+    [Route ("api/v1/[controller]")]
+    public class UserController : ControllerBase {
+
+        private readonly IMediator _mediator;
+
+        public UserController (IMediator mediator) {
+            _mediator = mediator;
+        }
+
+        #region GetUsers ()
+        [HttpGet]
+        [ProducesResponseType (typeof (IEnumerable<UserVm>), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<UserVm>>> GetUsers () {
+            var query = new GetUsersListQuery ();
+            var orders = await _mediator.Send (query);
+            return Ok (orders);
+        }
+        #endregion
+
+        #region GetUser ()
+        [HttpGet ("{id:length(24)}", Name = "GetUser")]
+        [ProducesResponseType (typeof (UserVm), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<UserVm>> GetUser (string id) {
+            var query = new GetUserQuery (id);
+            var orders = await _mediator.Send (query);
+            if (orders == null) {
+                return NotFound ();
+            }
+
+            return Ok (orders);
+        }
+        #endregion
+
+        #region CreaterUser ()
+        [HttpPost]
+        [ProducesResponseType (typeof (UserVm), (int) HttpStatusCode.OK)]
+        public async Task<ActionResult<UserVm>> CreateUser (CreateUserCommand command) {
+            var result = await _mediator.Send (command);
+            return Ok (result);
+        }
+        #endregion
+
+        #region UpdateUser ()
+        [HttpPut (Name = "UpdateUser")]
+        [ProducesResponseType (StatusCodes.Status204NoContent)]
+        [ProducesResponseType (StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> UpdateUser (UpdateUserCommand command) {
+            await _mediator.Send (command);
+            return NoContent ();
+        }
+        #endregion
+
+        #region DeleteUser ()
+        [HttpDelete ("{id:length(24)}", Name = "DeleteUser")]
+        [ProducesResponseType (StatusCodes.Status204NoContent)]
+        [ProducesResponseType (StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public async Task<ActionResult> DeleteUser (DeleteUserCommand command) {
+            await _mediator.Send (command);
+            return NoContent ();
+        }
+        #endregion
+    }
+}
--- ProjectExchange.API/Services/User/API/Startup.cs
+++ ProjectExchange.API/Services/User/API/Startup.cs
@@ -12,47 +12,39 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace API
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace API {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices (IServiceCollection services) {
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+            services.AddControllers ();
+            services.AddSwaggerGen (c => {
+                c.SwaggerDoc ("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+                app.UseSwagger ();
+                app.UseSwaggerUI (c => c.SwaggerEndpoint ("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            app.UseHttpsRedirection ();
 
-            app.UseRouting();
+            app.UseRouting ();
 
-            app.UseAuthorization();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
         }
     }
