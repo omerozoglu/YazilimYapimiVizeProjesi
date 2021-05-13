33bbd65889ce94672fbec437188f3c407f88ad0d
omerozoglu <omerozoglu.1@gmail.com>
2021-05-13 18:55:09 +0300

    :: Story Implementation, Component Header {-}

--- ProjectExchange.UI/ProjectExchange/src/app/components/header/header.component.ts
+++ ProjectExchange.UI/ProjectExchange/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public ProjectName = "Project: Exchange v1";
+  public isAuthenticated: boolean;
 
-  constructor() { }
-
+  constructor() {
+    //this.isAuthenticated = this.authenticationService.isAuthenticated();
+    this.isAuthenticated = true;
+  }
   ngOnInit(): void {
   }
 
+  logout() {
+    this.isAuthenticated = false;
+  }
 }
