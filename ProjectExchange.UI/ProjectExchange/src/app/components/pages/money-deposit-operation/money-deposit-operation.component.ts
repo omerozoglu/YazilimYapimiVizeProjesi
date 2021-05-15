164fd955e27bfa6f38ccbcff7b74fc062aff708b
omerozoglu <omerozoglu.1@gmail.com>
2021-05-15 19:51:46 +0300

    :: Story Implementation, Component Money Deposit Operation {-}

---
+++ ProjectExchange.UI/ProjectExchange/src/app/components/pages/money-deposit-operation/money-deposit-operation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-money-deposit-operation',
+  templateUrl: './money-deposit-operation.component.html',
+  styleUrls: ['./money-deposit-operation.component.scss']
+})
+export class MoneyDepositOperationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
