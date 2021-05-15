351f77b65a62e70868993633350f23ce689e6693
omerozoglu <omerozoglu.1@gmail.com>
2021-05-15 19:50:24 +0300

    :: Story Implementation, Component Sell Operation {-}

--- ProjectExchange.UI/ProjectExchange/src/app/components/pages/sell-operation/sell-operation.component.ts
+++ ProjectExchange.UI/ProjectExchange/src/app/components/pages/sell-operation/sell-operation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-sell-operation',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellOperationComponent implements OnInit {
 
-  constructor() { }
+  @Output() OperationName: string = "Sell";
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
